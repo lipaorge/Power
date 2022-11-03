1835d3181115de9e51424a5444744486cbdac145
MasteSan <milena.mozo98@gmail.com>
2022-11-03 23:34:48 +0100

    cambios en App.css y student list, create student components, clase del https://www.youtube.com/watch?v=IZA3smirwm8&ab_channel=Misi%C3%B3nTIC2022Ruta2-iMaster 25 oct

---
+++ src/components/student-list.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class StudentList extends Component {
+  render() {
+    return (
+      <div>create-student.component</div>
+    )
+  }
+}
