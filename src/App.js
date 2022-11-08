3ad5429d6af3c0beca5633907ed4ca89c6f692ba
MasteSan <milena.mozo98@gmail.com>
2022-11-08 16:11:51 +0100

    no changes

--- src/App.js
+++ src/App.js
@@ -30,13 +30,13 @@ function App() {
 
               <Nav className="justify-content-end">
                 <Nav>
-                  <Link to={"/create-student"} className="nav-link">
+                  <Link to={"/create-powerbank"} className="nav-link">
                     Crear Usuario
                   </Link>
                 </Nav>
 
                 <Nav>
-                  <Link to={"/student-list"} className="nav-link">
+                  <Link to={"/powerbank-list"} className="nav-link">
                     Listar Usuarios
                   </Link>
                 </Nav>
@@ -57,7 +57,6 @@ function App() {
                   />
                   <Route
                     exact
-<<<<<<< HEAD
                     path="/create-user"
                     component={(props) => <CreatePowerbank {...props} />}
                   />
@@ -71,20 +70,6 @@ function App() {
                     exact
                     path="/user-list"
                     component={(props) => <powerBankList {...props} />}
-=======
-                    path="/create-student"
-                    component={(props) => <CreateStudent {...props} />}
-                  />
-                  <Route
-                    exact
-                    path="/edit-student/:id"
-                    component={(props) => <EditStudent {...props} />}
-                  />
-                  <Route
-                    exact
-                    path="/student-list"
-                    component={(props) => <StudentList {...props} />}
->>>>>>> b68bd79d75777a77680916b020eca269178381bc
                   />
                 </Switch>
               </div>
